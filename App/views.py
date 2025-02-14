3535b2ebbd1f845910f80bc8d9e492a7ccd1204f
khushboo12vishwakarma <khushboovishwakarma677@gmail.com>
2025-02-14 03:13:05 -0800

    second commit

--- App/views.py
+++ App/views.py
@@ -1,25 +1,3 @@
-# # Create your views here.
-# from django.shortcuts import render,redirect
-# from django.core.paginator import Paginator
-# from . models import Song
-# from django.http import JsonResponse
-#
-#
-#
-# def index(request):
-#     paginator = Paginator(Song.objects.all(),1)
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     context={"page_obj":page_obj}
-#     return render(request,"main.html",context)
-#
-# def get_lyrics(request, song_id):
-#     try:
-#         song = Song.objects.get(id=song_id)
-#         return JsonResponse(song.lyrics_data)
-#     except Song.DoesNotExist:
-#         return JsonResponse({'error': 'Song not found'}, status=404)
-
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from .models import Song
